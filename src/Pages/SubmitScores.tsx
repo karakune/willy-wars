3f2b92b97b188e491b9afddb82a7c8cd05b00f11
karakune <hugopelletier783@gmail.com>
2025-02-17 18:35:33 -0500

    Submit Scores layout done

--- src/Pages/SubmitScores.tsx
+++ src/Pages/SubmitScores.tsx
@@ -1,21 +1,42 @@
 import "../App.css";
 import {Link} from "react-router";
+import PlayerBadge from "../Components/PlayerBadge.tsx";
+import {Player} from "../Models/Player.tsx";
+
+function PlayerEntry() {
+    return (
+        <div className="column" style={{alignItems:"center", minWidth: "0", gap:"2em"}}>
+            <PlayerBadge player={new Player()}/>
+            <input style={{paddingRight: "0", paddingLeft: "0", width: "4em", textAlign:"center", marginLeft:"3em"}}
+                   type="number" min="0" step="1"/>
+        </div>
+    )
+}
 
 export default function SubmitScores (){
     return (
-        <div>
+        <>
             <h1>Submit Scores</h1>
-            <div className="row">
-                <Link to="/RoundDisplay">
+            <div className="column" style={{gap:"5em", justifyContent:"center"}}>
+                <div className="row">
+                    <PlayerEntry/>
+                    <PlayerEntry/>
+                    <PlayerEntry/>
+                    <PlayerEntry/>
+                </div>
+                <div className="row" style={{justifyContent: "space-evenly"}}>
+                    <Link to="/WilliesDistribution">
+                        <button style={{fontSize: "large"}}>Round Over</button>
+                    </Link>
+                    <Link
+                        to="/FinalResults"> {/* TODO: choose between WilliesDistribution or FinalResults based on round #*/}
+                        <button style={{fontSize: "large"}}>Tourney Over</button>
+                    </Link>
+                </div>
+                <Link to="/RoundDisplay" style={{alignSelf:"flex-start", marginLeft: "3em"}}>
                     <button>Nevermind</button>
                 </Link>
-                <Link to="/WilliesDistribution">
-                    <button>Round Over</button>
-                </Link>
-                <Link to="/FinalResults"> {/* TODO: choose between WilliesDistribution or FinalResults based on round #*/}
-                    <button>Tourney Over</button>
-                </Link>
             </div>
-        </div>
+        </>
     );
 }
